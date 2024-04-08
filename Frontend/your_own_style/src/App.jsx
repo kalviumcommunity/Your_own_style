588585f8e6f2897975a6e79c9e24c67559426dbe
devasree A <devasree.a@kalvium.community>
2024-04-08 14:13:52 +0530

    created react-app

---
+++ Frontend/your_own_style/src/App.jsx
@@ -0,0 +1,24 @@
+
+import React from 'react';
+import './App.css'
+
+function App() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <img src="https://freedesignfile.com/upload/2017/10/Fashion-women-sign-with-logo-vectors-set-11.jpg" alt="Logo" className="logo" />
+        <ul className="nav-menu">
+          <li className="nav-item"><a href="#">Home</a></li>
+          <li className="nav-item"><a href="#">About</a></li>
+          <li className="nav-item"><a href="#">Register</a></li>
+          <li className="nav-item"><a href="#">Contact</a></li>
+          <li className="nav-item"><a href="#">Feedback</a></li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default App;
+
+
