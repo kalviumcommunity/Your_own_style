88bb7376b8fecf0c711ee61132e71d5c7adb700c
devasree A <devasree.a@kalvium.community>
2024-04-13 14:00:48 +0530

    added tops route with bg img

--- Frontend/your_own_style/src/components/top.jsx
+++ Frontend/your_own_style/src/components/top.jsx
@@ -7,9 +7,13 @@ import Navbar from './Navbar';
 function Top(){
   return(
     <div>
-      <Navbar />
-      <h1>About us</h1>
+      <h1>Here u can see the collections of top</h1>
+      <div className='blouseimg'>
+      <img src="https://i.pinimg.com/736x/d5/c1/d0/d5c1d04a937057dac414adc152896182.jpg" alt="" />
 
+
+      </div>
+     
     </div>
    
   )
