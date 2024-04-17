aee5cdb6b935361026daf467c455b5e416bc969b
devasree A <devasree.a@kalvium.community>
2024-04-17 14:37:17 +0530

    added images to occasions page

--- Frontend/your_own_style/src/components/Occasion.jsx
+++ Frontend/your_own_style/src/components/Occasion.jsx
@@ -1,13 +1,31 @@
 
 import React from 'react';
 import Navbar from './Navbar';
+import Lehenga from '../assets/Lehenga.jpeg'
+import './Occasion.css'
+import lehenga2 from '../assets/lehenga2.jpeg'
+import occasion from '../assets/occasion.jpeg'
+
+
 
 
 function Occasion(){
   return(
     <div>
       <Navbar />
-      <h1>forms</h1>
+      <div className='cover2'>
+        <div>
+        <img src={Lehenga} alt="" className='lehenga'/>
+
+          <img src={occasion} alt=""  className='oc'/>
+      
+
+      <img src={lehenga2} alt="" className='leh2' />
+      </div>
+
+
+      </div>
+     
     </div>
   )
 }
