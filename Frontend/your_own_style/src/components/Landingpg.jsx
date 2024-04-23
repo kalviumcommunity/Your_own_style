97c61783b9b662a32694bddba96eda8363ccd497
devasree A <devasree.a@kalvium.community>
2024-04-23 15:27:13 +0530

    creating footer

--- Frontend/your_own_style/src/components/Landingpg.jsx
+++ Frontend/your_own_style/src/components/Landingpg.jsx
@@ -15,6 +15,60 @@ function Landingpg() {
           <img src={siyaramlogo} alt="Siyaram" className="brand-logo-white-bg" />
           <img src={raymond} alt="Raymond" className="brand-logo" />
         </div>
+        <div className='footer'>
+          <div className='sb_footer section_padding'>
+            <div className='sb_footer-links-div1'>
+            
+
+              <div className='sb_footer-links_div2'>
+                <h4 className='title'>partners</h4>
+                <a href="/employe">
+                  <p className='yash'>Yash apparels</p>
+                </a>
+              </div>
+
+              <div className='sb_footer-links_div'>
+                <h4 className='text'>company</h4>
+               
+             
+                <a href="/about">
+                  <p className='t2'>about us</p>
+                </a>
+                <a href="/career">
+                  <p className='t3'>Help</p>
+                </a>
+                <a href="/contact">
+                  <p className='t4'>contact</p>
+                </a>
+
+              </div>
+
+              
+              <div className='socialmedia'>
+                <p><img src="{fb}" alt="" /></p>
+                <p><img src="{insta}" alt="" /></p>
+                <p><img src="{wht}" alt="" /></p>
+                <p><img src="{tw}" alt="" /></p>
+
+              </div>
+            </div>
+          </div>
+          <hr></hr>
+          <div className='sb_footer-below'>
+            <div className='sb_footer-copyright'>
+              <p>
+                @{new Date().getFullYear()} Your_own_style. All right reserved.
+              </p>
+            </div>
+            <div className='sb_footer-below-links'>
+              <a href="/terms"> <div><p>Terms and conditions</p></div></a>
+              <a href="/privacy"> <div><p>privacy</p></div></a>  
+              <a href="/secu"> <div><p>Security</p></div></a> 
+              <a href="/coookie"> <div><p>cookie declaration</p></div></a>     
+            </div>
+          </div>
+
+        </div>
       </div>
     </>
   );
