a4f6d1cdca503383f7694e1de23bc309ae7356c2
devasree A <devasree.a@kalvium.community>
2024-04-29 14:10:06 +0530

    linked signup page with home page

--- Frontend/your_own_style/src/App.jsx
+++ Frontend/your_own_style/src/App.jsx
@@ -2,13 +2,9 @@
 
 import React from 'react';
 import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
-import Home from './components/Home';
-import AboutUs from './components/AboutUs';
-import Forms from './components/Forms';
-import Top from './components/top';
-import Modal from './components/Modal';
-import Occasion from './components/Occasion';
+import Landingpg from './components/Landingpg';
 import Signup from './components/Signup';
+import Home from './components/Home';
 
 
 
@@ -16,24 +12,22 @@ import Signup from './components/Signup';
 
 function App() {
   return (
-    // <Router>
+      <Router>
 
-    //   <Routes>
-    //     <Route path="/home" element={<Home/>} />
-    //     <Route path='/top' element={<Top/>}/>
-    //     <Route path='/Modal'element={<Modal/>}/>
-    //     <Route path="/about" element={<AboutUs/>} />
-    //     <Route path='/Forms' element={<Forms/>} />
-    //     <Route path='/' element={<Signup/>}></Route>
-    //     <Route path='/Occasion'  element={<Occasion />}/>
+        <Routes>
+
+         <Route path='/' element={<Landingpg/>} />
+           <Route path='/signup' element={<Signup/>}></Route>
+           <Route path='/Home' element={<Home/>}></Route>
+      
         
      
       
 
 
-    //   </Routes>
-    // </Router>
-    <Signup/>
+        </Routes>
+       </Router>
+    
   );
 }
 
---
+++ Frontend/your_own_style/src/components/HomeData.jsx
@@ -0,0 +1,18 @@
+import redsaree from '../assets/red saree.webp';
+import casualimage2 from '../assets/casualimage2.jpg';
+import Frame3 from '../assets/Frame3.png';
+
+
+export const images = [
+  { title: "redsaree", subtitle: "How many faces do you have?", img: redsaree },
+  {
+    title: "casualimage2",
+    subtitle: "The best person to guide you is yourself",
+    img: casualimage2,
+  },
+  {
+    title: "frame3",
+    subtitle: "Why so serious?",
+    img: Frame3,
+  },
+];
