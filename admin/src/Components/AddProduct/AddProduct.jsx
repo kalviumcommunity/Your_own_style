a06814f6eaaff0840686a40cd348c340c0bd81ff
devasree A <devasree.a@kalvium.community>
2024-08-22 17:15:04 +0530

    link change

--- admin/src/Components/AddProduct/AddProduct.jsx
+++ admin/src/Components/AddProduct/AddProduct.jsx
@@ -28,7 +28,7 @@ const AddProduct = () => {
         let formData = new FormData();
         formData.append('product',image);
 
-        await fetch('http://localhost:4000/upload',{
+        await fetch('/upload',{
             method:'POST',
             headers:{
                 Accept:'application/json',
@@ -40,7 +40,7 @@ const AddProduct = () => {
         {
             product.image = responseData.image_url;
             console.log(product);
-            await fetch('http://localhost:4000/addproduct',{
+            await fetch('https://your-own-style-1.onrender.com/addproduct',{
                 method:'POST',
                 headers:{
                     Accept:'application/json',
